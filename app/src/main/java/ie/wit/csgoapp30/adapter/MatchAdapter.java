f1a96bec2089a0289c85767e3ef18d0f26399a97
caioarendt <arendtcaio@gmail.com>
2018-04-12 17:00:30 +0100

    Added splash + search!

--- app/src/main/java/ie/wit/csgoapp30/adapter/MatchAdapter.java
+++ app/src/main/java/ie/wit/csgoapp30/adapter/MatchAdapter.java
@@ -7,9 +7,14 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.widget.ArrayAdapter;
 import android.content.Context;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -30,17 +35,21 @@ public class MatchAdapter extends ArrayAdapter<Match> implements Filterable{
     private Context context;
     public List<Match> matches;
 
+    CustomFilter filter;
+    List<Match> filterList;
+
     public MatchAdapter(Context context, List<Match> matches){
         super(context, R.layout.row_match, matches);
         this.context = context;
         this.matches = matches;
+        this.filterList = matches;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent){
+    public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         final View view = inflater.inflate(R.layout.row_match, parent, false);
-        Match match  = matches.get(position);
+        Match match = matches.get(position);
         TextView teamsView = (TextView) view.findViewById(R.id.row_teams);
         TextView datetimeView = (TextView) view.findViewById(R.id.row_datetime);
 
@@ -67,9 +76,9 @@ public class MatchAdapter extends ArrayAdapter<Match> implements Filterable{
                             public void onClick(DialogInterface dialog, int id) {
                                 DatabaseHelper db = new DatabaseHelper(context);
                                 db.removeMatch(db.getMatch(view.getId()));
-                                if(context instanceof Activity){
-                                    ((Activity)context).finish();
-                                    ((Activity)context).startActivity(new Intent(((Activity)context), Main.class));
+                                if (context instanceof Activity) {
+                                    ((Activity) context).finish();
+                                    ((Activity) context).startActivity(new Intent(((Activity) context), Main.class));
                                 }
                             }
                         });
@@ -95,9 +104,9 @@ public class MatchAdapter extends ArrayAdapter<Match> implements Filterable{
             public void onClick(View v) {
                 DatabaseHelper db = new DatabaseHelper(context);
                 int matchid = view.getId();
-                Intent intent=new Intent(v.getContext(), Edit.class);
-                intent.putExtra("matchid",matchid);
-                if(context instanceof Activity) {
+                Intent intent = new Intent(v.getContext(), Edit.class);
+                intent.putExtra("matchid", matchid);
+                if (context instanceof Activity) {
                     ((Activity) context).finish();
                     v.getContext().startActivity(intent);
                 }
@@ -107,8 +116,70 @@ public class MatchAdapter extends ArrayAdapter<Match> implements Filterable{
         return view;
     }
 
-    public int getCount(){
+    @Override
+    public Filter getFilter() {
+        // TODO Auto-generated method stub
+        if(filter == null)
+        {
+            filter=new CustomFilter();
+        }
+
+        return filter;
+    }
+
+    class CustomFilter extends Filter
+    {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            // TODO Auto-generated method stub
+
+            FilterResults results=new FilterResults();
+
+            if(constraint != null && constraint.length()>0)
+            {
+                //CONSTARINT TO UPPER
+                constraint=constraint.toString().toUpperCase();
+
+                ArrayList<Match> filters=new ArrayList<Match>();
+
+                //get specific items
+                for(int i=0;i<filterList.size();i++)
+                {
+                    if(filterList.get(i).getTeam1().toUpperCase().contains(constraint) || filterList.get(i).getTeam2().toUpperCase().contains(constraint) || filterList.get(i).getDate().toUpperCase().contains(constraint) || filterList.get(i).getTime().toUpperCase().contains(constraint))
+                    {
+                        Match p=new Match(filterList.get(i).getTeam1(), filterList.get(i).getTeam2(), filterList.get(i).getDate(), filterList.get(i).getTime());
+
+                        filters.add(p);
+                    }
+                }
+
+                results.count=filters.size();
+                results.values=filters;
+
+            }else
+            {
+                results.count=filterList.size();
+                results.values=filterList;
+
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            // TODO Auto-generated method stub
+
+            matches=(ArrayList<Match>) results.values;
+            notifyDataSetChanged();
+        }
+
+    }
+
+    public int getCount() {
         return matches.size();
     }
+
 
 }
